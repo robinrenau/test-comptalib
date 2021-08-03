61e5b97094e4ee95c2da02dcf9156c953cf7d352
robinrenau <robinrenau@gmail.com>
2021-08-03 16:03:07 +0200

    hero test and animation

--- src/pages/Hero.vue
+++ src/pages/Hero.vue
@@ -1,8 +1,30 @@
 <template>
   <div>
     <site-header></site-header>
-      <h1>About us</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+      <lottie-animation
+      path='./70662-olympics-run-animation-tokyo-2021.json'
+      :loop="false"
+      :autoPlay="true"
+      :loopDelayMin="2.5"
+      :loopDelayMax="5"
+      :speed="1"
+      :width="256"
+      :height="256"
+      @AnimControl="setAnimController"
+  />
     <site-footer></site-footer>
   </div>
 </template>
+
+<script>
+import LottieAnimation from 'lottie-vuejs' // import lottie-vuejs
+
+export default {
+  components: {
+      LottieAnimation
+  },
+  data: () => ({
+    
+  })
+};
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,7 +5,8 @@
             <div class="container pb-20 px-3 mx-auto items-center">
                 <section class="container mx-auto pt-6">
                     <slot name="main-content" />
-                    <g-link class="nav__link" to="/hero/">Cliquez ici pour voir le Hero </g-link>
+                    <g-link style="margin-right:10px" to="/hero/">Cliquez ici pour voir le Hero </g-link>
+                    <g-link to="/section/">Cliquez ici pour voir le composant de section </g-link>
                 </section>
             </div>
         </main>
